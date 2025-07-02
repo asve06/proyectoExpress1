51137ef38eebf63e50f11eb991418f281baf1bf1
asve06 <asvega06@gmail.com>
2025-07-01 19:12:49 -0500

    Agregar vista, servicio y tabla con lectura de fotos

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var axios = require("axios");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.get("/photos", async function (req, res, next) {
+  const URL = "https://dawm-fiec-espol-default-rtdb.firebaseio.com/photos.json";
+
+  const response = await axios.get(URL);
+  res.render("fotos", { title: "Fotos", fotos: response.data });
 });
 
 module.exports = router;
